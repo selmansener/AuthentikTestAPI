12f47692782aaf6d4f810faa225647e0786f4557
Ahmet Selman Şener <selmansenerr@gmail.com>
2025-06-07 12:55:48 +0300

    Add JWT authentication with Authentik

--- AuthentikTestAPI/Program.cs
+++ AuthentikTestAPI/Program.cs
@@ -14,6 +14,39 @@ namespace AuthentikTestAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services
+                .AddAuthentication(Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // Discovery will pull issuer, signing keys and token endpoints from
+                    // https://auth.hyperc.tr/.well-known/openid-configuration
+                    // Access tokens must be signed JWTs (JWS). Encrypted JWE tokens will fail to validate.
+                    options.Authority = "https://auth.hyperc.tr";
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        // Validate the JWT issuer and audience
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = "b2fS6rmY8JzD80iVplmaBq6ylM6xzKi73nEh9TVd",
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true
+                    };
+
+                    // Custom error response for unauthorized requests
+                    options.Events = new Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerEvents
+                    {
+                        OnChallenge = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                            context.Response.ContentType = "application/json";
+                            return context.Response.WriteAsync("{\"error\":\"Unauthorized\"}");
+                        }
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -24,6 +57,9 @@ namespace AuthentikTestAPI
             }
 
             app.UseHttpsRedirection();
+
+            // Process authentication/authorization for incoming requests
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
